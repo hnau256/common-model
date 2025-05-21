a5aeb836239a27e9db54f077cee4e3e49fa5b34e
Mark Zorkhin <mark.zorikhin@dif.tech>
2025-05-21 22:15:49 +0300

    v1.0.4

---
+++ src/main/kotlin/hnau/common/model/color/ThemeBrightness.kt
@@ -0,0 +1,9 @@
+package hnau.common.model.color
+
+enum class ThemeBrightness {
+    Light,
+    Dark,
+    ;
+
+    companion object
+}
--- src/main/kotlin/hnau/common/model/preferences/PreferenceExt.kt
+++ src/main/kotlin/hnau/common/model/preferences/PreferenceExt.kt
@@ -1,8 +1,8 @@
 package hnau.common.model.preferences
 
 import arrow.core.Option
-import arrow.core.identity
-import arrow.core.some
+import arrow.core.Some
+import arrow.core.getOrElse
 import hnau.common.kotlin.coroutines.mapState
 import hnau.common.kotlin.mapper.Mapper
 import kotlinx.coroutines.CoroutineScope
@@ -10,25 +10,35 @@ import kotlinx.coroutines.CoroutineScope
 fun <I, O> Preference<Option<I>>.map(
     scope: CoroutineScope,
     mapper: Mapper<I, O>,
-): Preference<Option<O>> = Preference<Option<O>>(
+): Preference<Option<O>> = mapOption(
+    scope = scope,
+    mapper = Mapper(
+        direct = { it.map(mapper.direct) },
+        reverse = { it.map(mapper.reverse) }
+    )
+)
+
+fun <I, O> Preference<Option<I>>.mapOption(
+    scope: CoroutineScope,
+    mapper: Mapper<Option<I>, O>,
+): Preference<O> = Preference<O>(
     value = value.mapState(scope) { valueOrNone ->
-        valueOrNone.map(mapper.direct)
+        mapper.direct(valueOrNone)
     },
     update = { newValue ->
-        val transformedNewValue = newValue.map(mapper.reverse)
-        update(transformedNewValue)
+        update(newValue.let(mapper.reverse))
     }
 )
 
 inline fun <T> Preference<Option<T>>.withDefault(
     scope: CoroutineScope,
     crossinline default: () -> T,
-): Preference<T> = Preference<T>(
-    value = value.mapState(scope) { valueOrNone ->
-        valueOrNone.fold(
-            ifEmpty = default,
-            ifSome = ::identity
-        )
-    },
-    update = { newValue -> update(newValue.some()) }
+): Preference<T> = mapOption(
+    scope = scope,
+    mapper = Mapper(
+        direct = { valueOrNone ->
+            valueOrNone.getOrElse(default)
+        },
+        reverse = ::Some
+    )
 )
